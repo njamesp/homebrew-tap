109aec4ab2a61ca4f0cb36418a0de1420cdf41b6
Nick Parrish <nick.parrish@numerica.us>
2016-01-22 20:45:33 -0700

    Added formulas for boost 1.47 to 1.60

--- boost1_51.rb
+++ boost1_51.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 #Builds Boost with clang and c++11
 
-class Boost150 < Formula
+class Boost151 < Formula
   homepage 'http://boost.org'
-  url 'http://sourceforge.net/projects/boost/files/boost/1.50.0/boost_1_50_0.tar.gz'
+  url 'http://sourceforge.net/projects/boost/files/boost/1.51.0/boost_1_51_0.tar.gz'
 
   def install
-    system "./bootstrap.sh", "--libdir=#{HOMEBREW_PREFIX}/Boost/1.50/lib/", "--includedir=#{HOMEBREW_PREFIX}/Boost/1.50/include/", "--with-python=#{HOMEBREW_PREFIX}/bin/python"
+    system "./bootstrap.sh", "--libdir=#{HOMEBREW_PREFIX}/Boost/1.51/lib/", "--includedir=#{HOMEBREW_PREFIX}/Boost/1.51/include/", "--with-python=#{HOMEBREW_PREFIX}/bin/python"
 
     system "./b2", "toolset=clang", "cxxflags=-std=c++0x -stdlib=libc++", "linkflags=-stdlib=libc++", "--without-mpi", "--without-signals", "variant=release", "threading=multi", "install"
   end
--- boost1_55.rb
+++ boost1_55.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 #Builds Boost with clang and c++11
 
-class Boost150 < Formula
+class Boost155 < Formula
   homepage 'http://boost.org'
-  url 'http://sourceforge.net/projects/boost/files/boost/1.50.0/boost_1_50_0.tar.gz'
+  url 'http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz'
 
   def install
-    system "./bootstrap.sh", "--libdir=#{HOMEBREW_PREFIX}/Boost/1.50/lib/", "--includedir=#{HOMEBREW_PREFIX}/Boost/1.50/include/", "--with-python=#{HOMEBREW_PREFIX}/bin/python"
+    system "./bootstrap.sh", "--libdir=#{HOMEBREW_PREFIX}/Boost/1.55/lib/", "--includedir=#{HOMEBREW_PREFIX}/Boost/1.55/include/", "--with-python=#{HOMEBREW_PREFIX}/bin/python"
 
     system "./b2", "toolset=clang", "cxxflags=-std=c++0x -stdlib=libc++", "linkflags=-stdlib=libc++", "--without-mpi", "--without-signals", "variant=release", "threading=multi", "install"
   end
