4da130e48e04afafc7dc62641ff5368bffb7df7d
Nick Parrish <nick.parrish@numerica.us>
2017-11-20 21:30:00 -0700

    Added boost1_63.

---
+++ boost1_63.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+#Builds Boost with clang and c++11
+
+class Boost163 < Formula
+  homepage 'http://boost.org'
+  url 'http://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.gz'
+  sha256 'fe34a4e119798e10b8cc9e565b3b0284e9fd3977ec8a1b19586ad1dec397088b'
+
+  def install
+    system "./bootstrap.sh", "--libdir=#{prefix}/lib/", "--includedir=#{prefix}/include/", "--with-python=#{HOMEBREW_PREFIX}/bin/python"
+
+    system "./b2", "toolset=clang", "cxxflags=-std=c++17 -stdlib=libc++", "linkflags=-stdlib=libc++", "--without-mpi", "--without-signals", "variant=release", "threading=multi", "install"
+  end
+end
+
